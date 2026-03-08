f1acd1d5472a79199ab857ed4989018cb2bd91a1
beki-github <bereketendale9@gmail.com>
2026-03-08 18:12:33 -0300

    intalized the project

---
+++ scripts/circle.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mouse_entered() -> void:
+	print("in")
