98a584d36d9dec544553fad252da1cc16624baf5
beki-github <bereketendale9@gmail.com>
2026-03-12 04:27:26 -0300

    fixed the duplicated node error

--- scripts/circle.gd
+++ scripts/circle.gd
@@ -1,15 +1,6 @@
 extends Area2D
 signal  cleared
 
-var x:=4
-
-
-
-
-func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		print("circle listen to your event")
-		x+=1
-		print(x)
-		cleared.emit()
-		queue_free()
+func _on_mouse_entered() -> void:
+	cleared.emit()
+	queue_free()
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 var circle_node = preload("res://scenes/circle.tscn")
 
+var rng=RandomNumberGenerator.new()
+
 func _ready() -> void:
 	spawn_circle()
 
 func spawn_circle():
 	var new_circle = circle_node.instantiate()
 	new_circle.cleared.connect(_on_circle_cleared)
+	new_circle.position=Vector2i(rng.randi_range(-370,370),rng.randi_range(-270,270))
 	add_child(new_circle)
 
 func _on_circle_cleared():
