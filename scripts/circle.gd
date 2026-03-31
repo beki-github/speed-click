46562e501e3ded3fcbf15dfa827ad54a03be6995
beki-github <bereketendale9@gmail.com>
2026-03-31 19:39:12 -0300

    fixed a duplication errror and added a wall scene for bouncing effect

--- scripts/circle.gd
+++ scripts/circle.gd
@@ -1,8 +1,12 @@
 extends Area2D
 signal cleared
 
+var is_hit:=false
 
 func _on_area_entered(area: Area2D) -> void:
+	if is_hit:
+		return
 	if area.is_in_group("tail_group"):
+		is_hit=true
 		cleared.emit()
 		queue_free()
