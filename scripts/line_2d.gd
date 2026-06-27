ad188bb17a0455f6ca120677b164013b23489b0a
beki-github <bereketendale9@gmail.com>
2026-06-27 14:30:02 +0000

    improved the ray effects

--- scripts/line_2d.gd
+++ scripts/line_2d.gd
@@ -1,14 +1,20 @@
 extends Line2D
-
+var heightParent: float
+var widthParent: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	heightParent =get_parent().size.y/2
+	widthParent =get_parent().size.x/2
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	add_point(get_parent().global_position)
-	if points.size()>20:
+func _process(_delta: float) -> void:
+	var currentPosition:Vector2=get_parent().global_position
+	currentPosition.x+=heightParent
+	currentPosition.y+=heightParent
+	add_point(currentPosition)
+	if points.size()>25:
 		remove_point(0)
 	pass
